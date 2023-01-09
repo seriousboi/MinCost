52c8b2d7c117a916b57d9f27cbf6490100e21218
seriousboi <ppinet9@gmail.com>
2023-01-09 18:53:27 +0100

    Ajout de la méthode pour augmenter le flot. Passage en méthode de plusieurs fonctions. Adaptations des classes aux changements.

--- graph.cpp
+++ graph.cpp
@@ -14,7 +14,10 @@ class Arc
     float capacity;
     float residualCapacity;
 
+    int pairId; /* indice de l'arc inverse parmis le vecteur d'arcs qui vont dans l'autre sens */
+
     Arc(float capacity,float cost);
+    void print();
 };
 
 
@@ -26,20 +29,136 @@ class Graph
     vector<int> productions;
     vector<unordered_map<int,vector<Arc>>> vertices;
 
+    int sourceIndex; /* identifiants du la super source et du super puit si il existent, -1 sinon */
+    int sinkIndex;
+
     Graph(int nbVertices);
     Graph(string filename);
+    void addSuperSourceSink();
+    void symmetrisation();
     void addArc(int start,int end,float capacity,float cost);
+    void increaseFlow(float increase,int start,int end);
+    bool isFlowPassing(int start,int end);
     void print();
 };
 
 
 
-Arc::Arc(float capacity,float cost)
+void Graph::increaseFlow(float increase,int start,int end)
 {
-  this->capacity = capacity;
-  this->cost = cost;
-  flow = 0;
-  residualCapacity = capacity;
+  if(increase == 0){return;} /* pour éviter les boucles infinies */
+
+  if(not isFlowPassing(end,start)) /*on vérifie que du flot ne passe pas dans l'autre sens */
+  {
+    for(Arc & arc:vertices[start][end]) /* on cherche l'arc sur lequel faire passer le flot */
+    {
+      if(arc.capacity > 0) /* /!\ on fait la supposition qu'il existe un unique arc avec capacity > 0 entre start et end*/
+      {
+        arc.flow += increase;
+        arc.residualCapacity -= increase;
+        Arc & pairedEdge = vertices[end][start][arc.pairId];
+        pairedEdge.residualCapacity += increase;
+        return;
+      }
+    }
+  }
+  else
+  { /* appels récursifs infinis si les deux flots sont non-nuls */
+    for(Arc & arc:vertices[end][start]) /* on cherche l'arc sur lequel du flot passe */
+    {
+      if(arc.flow > 0) /* /!\ on fait la supposition qu'il existe un unique arc avec flow > 0 entre end et start*/
+      {
+        float flowReversed = min(arc.flow,increase);
+        increaseFlow(-flowReversed,end,start);
+        increaseFlow(increase-flowReversed,start,end);
+        return;
+      }
+    }
+  }
+}
+
+
+
+bool Graph::isFlowPassing(int start,int end) /* vérifie si du flot passe entre les sommets start et end, spécifiquement dans ce sens */
+{
+  for(Arc arc:vertices[start][end])
+  {
+    if(arc.flow > 0)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
+void Graph::addArc(int start,int end,float capacity,float cost) /* ajoute l'arc du sommet start vers le sommet end */
+{
+  if(not(start<nbVertices and end<nbVertices and start>=0 and end>=0))
+  {
+    cout << "Cannot add invalid edge to graph, yikes\n";
+    return;
+  }
+  vertices[start][end].push_back(Arc(capacity,cost));
+}
+
+
+
+void Graph::addSuperSourceSink()
+{
+  // 2 variables qui vont nous permettre d'avoir la production et la consommation totale
+  int produit = 0;
+  int consomme = 0;
+  // on crée les noeuds source et puit
+  unordered_map<int,vector<Arc>> source;
+  unordered_map<int,vector<Arc>> puit;
+  // on les ajoute à notre graphe (aux deux dernières positions)
+  vertices.push_back(source);
+  vertices.push_back(puit);
+  sourceIndex = nbVertices;
+  sinkIndex = nbVertices+1;
+  nbVertices += 2;
+
+  for (int i=0; i< nbVertices-2; ++i){   //on parcourt nootre vecteur productions
+    if (productions[i]>0)
+    {                     // on récupére les noeuds producteurs
+      addArc(sourceIndex,i,productions[i],0);    //création des arcs du 'super' noeud vers les 'vrai' noeuds producteurs
+      produit = produit + productions[i];
+      productions[i] = 0;
+    }
+    else if (productions[i]<0)
+    {                // on récupère les noeuds consommateurs
+      consomme = consomme + productions[i];
+      addArc(i,sinkIndex,productions[i],0);  // création des arcs des 'vrai' noeud vers le 'super' noeud consommateur
+      productions[i] = 0;
+    }
+  }
+  // on ajoute les productions totales au vecteur des productions
+  productions.push_back(produit);
+  productions.push_back(consomme);
+}
+
+
+
+void Graph::symmetrisation()
+{
+  for(int vertex=0;vertex<nbVertices;++vertex)
+  {
+    for(auto & [neighbor,arcs] : vertices[vertex])
+    {
+      for(int arcIndex = 0;arcIndex<arcs.size();++arcIndex)
+      { /* on ajoute l'arc inverse même si il y a un arc qui va dans le sens oppsé à cause des coûts */
+        Arc & arc = vertices[vertex][neighbor][arcIndex];
+        if(arc.capacity > 0)
+        { /* en revanche on vérifie si la capacité est strictement positive pour ne pas donné d'arc inverse aux arcs inverses */
+          addArc(neighbor,vertex,0,-arc.cost);
+          arc.pairId = vertices[neighbor][vertex].size()-1; /* le sous index de l'arc inverse et la taille du vecteur qui le contient -1 car on vient juste d'ajouter l'arc au vecteur */
+          vertices[neighbor][vertex].back().pairId = arcIndex;
+        }
+      }
+    }
+  }
 }
 
 
@@ -52,6 +171,10 @@ Graph::Graph(string filename)
     std::cout << "oof, cannot open file " << filename << std::endl;
     return;
   }
+
+  sourceIndex = -1;
+  sinkIndex = -1;
+
   string lineIdentifier;
   dataStream >> lineIdentifier;
   while(not dataStream.eof())
@@ -86,7 +209,6 @@ Graph::Graph(string filename)
       addArc(startIndex,endIndex,capacity,cost);
     }
     dataStream >> lineIdentifier;
-    cout<<lineIdentifier<<endl;
   }
 }
 
@@ -96,18 +218,8 @@ Graph::Graph(int nbVertices)
 {
   this->nbVertices = nbVertices;
   vertices = std::vector<std::unordered_map<int,vector<Arc>>>(nbVertices);
-}
-
-
-
-void Graph::addArc(int start,int end,float capacity,float cost) /* ajoute l'arc du sommet start vers le sommet end */
-{
-  if(not(start<nbVertices and end<nbVertices and start>=0 and end>=0))
-  {
-    cout << "Cannot add invalid edge to graph, yikes\n";
-    return;
-  }
-  vertices[start][end].push_back(Arc(capacity,cost));
+  sourceIndex = -1;
+  sinkIndex = -1;
 }
 
 
@@ -117,15 +229,42 @@ void Graph::print() /* affiche les listes d'adjacences sous forme <sommet i: (vo
   std::cout << "graph with " << nbVertices << " vertices\n";
   for(int vertex=0;vertex<nbVertices;++vertex)
   {
-    std::cout << "vertex " << vertex << ": ";
+    std::cout << "vertex " << vertex;
+    if(productions[vertex]>0)
+    {
+      std::cout << " +(" << productions[vertex] << ")";
+    }
+    if(productions[vertex]<0)
+    {
+      std::cout << " -(" << -productions[vertex] << ")";
+    }
+    std::cout<< ": ";
 
     for(auto &[neighbor,arcs] : vertices[vertex])
     {
       for(Arc & arc : arcs)
       {
-        std::cout << "(" << neighbor << "," << arc.capacity << ") ";
+        std::cout << "(" << neighbor << "," << arc.capacity << "," << arc.cost << ") ";
       }
     }
     std::cout << "\n";
   }
+}
+
+
+
+Arc::Arc(float capacity,float cost)
+{
+  this->capacity = capacity;
+  this->cost = cost;
+  flow = 0;
+  residualCapacity = capacity;
+  pairId = -1;
+}
+
+
+
+void Arc::print() /* affiche les détails d'un arc, mais n'affiche pas le sommet sortant et le sommet entrant */
+{
+  std::cout << "(flow:" << flow << ", cap:" << capacity << ", rescap:" << residualCapacity << ", cost:" << cost << ", pairId:" << pairId << ") " << endl;
 }
--- mincost.cpp
+++ mincost.cpp
@@ -2,13 +2,91 @@
 
 
 
-bool checkGraph(Graph & graph);  /* retourne un booléen qui indique si les flots sont valides */
+bool checkGraph(Graph & graph);  /* retourne un booléen qui indique si les flots sont valides et affiche le cout */
+float cost_sol(Graph & graph);   //retourne le cout de la solution retournée
+Graph pccs(Graph & graph);       // retourne un graphe avec un graphe de cout min
 
 
 
 int main(int nbArgs,char **values)
 {
-  Graph testGraph("instances/example.dat");
-  testGraph.print();
-  return 0;
+  Graph test("instances/example.dat");
+  test.print();
+  test.addSuperSourceSink();
+  test.print();
 }
+
+
+
+bool checkGraph(Graph & graph){
+  float flot_entrant = 0;
+  float flot_sortant = 0;
+
+  for (int i =0; i< graph.nbVertices; ++i) {        // parcours du vecteur des noeuds départs
+    for(auto &[k, v] :graph.vertices[i]) {          // parcours de la map des noeuds destinations et des arcs associés
+      for (int i=0; i< v.size(); ++i){       // parcours du vecteur des arcs destinations (si il y a plusieurs éléments dans ce vecteur c'est que le graphe est un multigraphe)
+        if ( v[i].capacity<=v[i].flow){       // le flot des arcs doit être  inférieur à leur capacité (2ème contraintes)
+          return false;
+        }
+
+        flot_sortant = flot_sortant + v[i].capacity;    // flot sortant du noeud i
+        for (auto &[l, w] :graph.vertices[k]){                // parcours des arcs symétriques au noeud sortant de i,  i.e. les noeuds entrants en i (pour que cela marche il faut que le graphe soit symmétrique )
+          for (int j=0; j< w.size(); ++j){              // parcours du vecteur des arcs de ces noeuds
+            flot_entrant = flot_entrant + w[j].flow;
+          }
+        }
+      }
+    }
+  if( flot_entrant != flot_sortant){
+    return false;
+  }
+  }
+  return true;
+}
+
+
+
+float cost_sol(Graph & graph){
+  float cost_tot = 0;
+  for (int i =0; i< graph.nbVertices; ++i) {        // parcours du vecteur des noeuds départs
+    for(auto &[k, v] :graph.vertices[i]) {          // parcours de la map des noeuds destinations et des arcs associés
+      for (int i=0; i< v.size(); ++i){
+        cost_tot = cost_tot + v[i].cost*v[i].flow;     //on ajoute les couts
+      }
+    }
+  }
+  return cost_tot;
+}
+
+
+
+// Graph pccs(Graph & graph, vector<int> consommation){
+//   vector<int> potentiel (graph.nbVertices,0);
+//   symmetrisation(graph);
+//   while (consommation[source])!=0){                             // tant qu'on a du flot à envoyer b(s) je sais pas comment on l'obtient
+//     vector<int> distance = dijkstra(s,graph);   // On calucle la distance entre s et chaque noeud
+
+
+//     //mise à jour des potentiels
+//     for (int i=0; i< distance.size(); ++i){
+//       potentiel[i] = potentiel[i] - distance[i];
+//     }
+
+
+//     //calcul de la valeur du flow
+
+//     float delta = b(s);
+//     if (b(p)<delta)){
+//       delta = b(p);
+//     }
+//     for (int i =0; i< graph.nbVertices; ++i) {        // parcours du vecteur des noeuds départs
+//       for(auto &[k, v] :graph.vertices[i]) {          // parcours de la map des noeuds destinations et des arcs associés
+//         for (int i=0; i< v.size(); ++i){
+//           if(v[i].residualCapacity<delta){
+//             delta = v[i].residualCapacity<delta
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
